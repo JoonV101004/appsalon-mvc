22f11bf757b992543cd4a368be7253bcda994b83
Joon Vargas <vjoondominic@gmail.com>
2025-09-23 19:08:20 -0600

    Corrige rutas y actualiza Gulp para producción

---
+++ gulpfile.cjs
@@ -0,0 +1,77 @@
+const path = require('path');
+const fs = require('fs');
+const { glob } = require('glob');
+const { src, dest, watch, series, parallel } = require('gulp');
+const dartSass = require('sass');
+const gulpSass = require('gulp-sass')(dartSass);
+const terser = require('gulp-terser');
+const sharp = require('sharp');
+
+const paths = {
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js'
+};
+
+function css(done) {
+    src(paths.scss, { sourcemaps: true })
+        .pipe(gulpSass({
+            outputStyle: 'compressed'
+        }).on('error', gulpSass.logError))
+        .pipe(dest('./public/build/css', { sourcemaps: '.' }));
+    done();
+}
+
+function js(done) {
+    src(paths.js)
+        .pipe(terser())
+        .pipe(dest('./public/build/js'));
+    done();
+}
+
+async function imagenes(done) {
+    const srcDir = './src/img';
+    const buildDir = './public/build/img';
+    const images = await glob('./src/img/**/*');
+
+    images.forEach(file => {
+        const relativePath = path.relative(srcDir, path.dirname(file));
+        const outputSubDir = path.join(buildDir, relativePath);
+        procesarImagenes(file, outputSubDir);
+    });
+    done();
+}
+
+function procesarImagenes(file, outputSubDir) {
+    if (!fs.existsSync(outputSubDir)) {
+        fs.mkdirSync(outputSubDir, { recursive: true });
+    }
+    const baseName = path.basename(file, path.extname(file));
+    const extName = path.extname(file);
+
+    if (extName.toLowerCase() === '.svg') {
+        const outputFile = path.join(outputSubDir, `${baseName}${extName}`);
+        fs.copyFileSync(file, outputFile);
+    } else {
+        const outputFile = path.join(outputSubDir, `${baseName}${extName}`);
+        const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`);
+        const outputFileAvif = path.join(outputSubDir, `${baseName}.avif`);
+        const options = { quality: 80 };
+
+        sharp(file).jpeg(options).toFile(outputFile);
+        sharp(file).webp(options).toFile(outputFileWebp);
+        sharp(file).avif().toFile(outputFileAvif);
+    }
+}
+
+function dev() {
+    watch(paths.scss, css);
+    watch(paths.js, js);
+    watch('src/img/**/*.{png,jpg}', imagenes);
+}
+
+// Exportar tareas
+exports.css = css;
+exports.js = js;
+exports.imagenes = imagenes;
+exports.dev = dev;
+exports.default = series(js, css, imagenes, dev);
