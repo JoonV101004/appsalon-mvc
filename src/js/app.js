fc0479b485b180fce5cea8c65b3b6677fc7ad036
Joon Vargas <vjoondominic@gmail.com>
2025-09-23 20:02:32 -0600

    Corrige

--- src/js/app.js
+++ src/js/app.js
@@ -115,9 +115,9 @@ function paginaSiguiente() {
 }
 
 async function consultarAPI() {
-
+ const url = '/api/servicios';
     try {
-        const url = '/api/servicios';
+        //const url = '/api/servicios';
         const resultado = await fetch(url);
         const servicios = await resultado.json();
         mostrarServicios(servicios);
