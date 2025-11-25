56979435534f3876298acb3e40b0c00cb4e236e9
Joon Vargas <vjoondominic@gmail.com>
2025-11-25 09:43:42 -0600

    Solución a bug de no cierre de sesión

--- includes/funciones.php
+++ includes/funciones.php
@@ -26,6 +26,11 @@ function isAuth() : void {
     if(!isset($_SESSION['login'])) {
         header('Location: /');
     }
+
+    // Dicen al navegador: "No guardes nada en caché"
+    header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+    header("Pragma: no-cache"); // HTTP 1.0.
+    header("Expires: 0"); // Proxies.
 }
 
 function isAdmin() : void {
