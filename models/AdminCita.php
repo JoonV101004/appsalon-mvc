68f6d6c04b7870c0a230159ea41cf4ea3d57661c
Uriel Rosales <rosalesu901@gmail.com>
2025-12-18 15:22:11 -0600

    admincita

--- models/AdminCita.php
+++ models/AdminCita.php
@@ -4,24 +4,38 @@ namespace Model;
 
 class AdminCita extends ActiveRecord {
     protected static $tabla = 'citasServicios';
-    protected static $columnasDB = ['id', 'hora', 'cliente', 'email', 'telefono', 'servicio', 'precio'];
+    protected static $columnasDB = [
+        'id', 
+        'fecha',   // ✅ agregado
+        'hora', 
+        'cliente', 
+        'email', 
+        'telefono', 
+        'servicio', 
+        'precio', 
+        'estado'   // ✅ agregado
+    ];
 
     public $id;
+    public $fecha;
     public $hora;
     public $cliente;
     public $email;
     public $telefono;
     public $servicio;
     public $precio;
+    public $estado;
 
-    public function __construct()
+    public function __construct($args = [])
     {
         $this->id = $args['id'] ?? null;
+        $this->fecha = $args['fecha'] ?? '';
         $this->hora = $args['hora'] ?? '';
         $this->cliente = $args['cliente'] ?? '';
         $this->email = $args['email'] ?? '';
         $this->telefono = $args['telefono'] ?? '';
         $this->servicio = $args['servicio'] ?? '';
         $this->precio = $args['precio'] ?? '';
+        $this->estado = $args['estado'] ?? 'activa';
     }
 }
